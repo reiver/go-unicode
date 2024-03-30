c139acd520cdd5f848023ba0b035265d25199070
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-30 13:20:11 -0700

    initial commits

--- names.go
+++ names.go
@@ -2,44 +2,77 @@ package unicode
 
 // ASCII names.
 const (
-	Null                    =  NUL
-	StartOfHeading          =  SOH
-	StartOfText             =  STX
-	EndOfText               =  ETX
-	EndOfTransmission       =  EOT
-	Enquiry                 =  ENQ
-	Acknowledgment          =  ACK
-	Bell                    =  BEL
-	Backspace               =  BS
-	HorizontalTab           =  HT
-	LineFeed                =  LF
-	VerticalTab             =  VT
-	FormFeed                =  FF
-	CarriageReturn          =  CR
-	ShiftOut                =  SO
-	ShiftIn                 =  SI
-	DataLinkEscape          =  DLE
-	DeviceControl1          =  DC1
-	DeviceControl2          =  DC2
-	DeviceControl3          =  DC3
-	DeviceControl4          =  DC4
-	NegativeAcknowledgement =  NAK
-	SynchronousIdle         =  SYN
-	EndOfTransmitBlock      =  ETB
-	Cancel                  =  CAN
-	EndOfMedium             =  EM
-	Substitute              =  SUB
-	Escape                  =  ESC
-	FileSeparator           =  FS
-	GroupSeparator          =  GS
-	RecordSeparator         =  RS
-	UnitSeparator           =  US
-	Space                   =  SP
+	Null                                 rune =  NUL
+	StartOfHeading                       rune =  SOH
+	StartOfText                          rune =  STX
+	EndOfText                            rune =  ETX
+	EndOfTransmission                    rune =  EOT
+	Enquiry                              rune =  ENQ
+	Acknowledgment                       rune =  ACK
+	Bell                                 rune =  BEL
+	Backspace                            rune =  BS
+	HorizontalTab                        rune =  HT
+	LineFeed                             rune =  LF
+	VerticalTab                          rune =  VT
+	FormFeed                             rune =  FF
+	CarriageReturn                       rune =  CR
+	ShiftOut                             rune =  SO
+	ShiftIn                              rune =  SI
+	DataLinkEscape                       rune =  DLE
+	DeviceControl1                       rune =  DC1
+	DeviceControl2                       rune =  DC2
+	DeviceControl3                       rune =  DC3
+	DeviceControl4                       rune =  DC4
+	NegativeAcknowledgement              rune =  NAK
+	SynchronousIdle                      rune =  SYN
+	EndOfTransmitBlock                   rune =  ETB
+	Cancel                               rune =  CAN
+	EndOfMedium                          rune =  EM
+	Substitute                           rune =  SUB
+	Escape                               rune =  ESC
+	FileSeparator                        rune =  FS
+	GroupSeparator                       rune =  GS
+	RecordSeparator                      rune =  RS
+	UnitSeparator                        rune =  US
+	Space                                rune =  SP
 	// ...
-	Delete                  = DEL
+	Delete                               rune = DEL
+	// ---
+	PaddingCharacter                     rune = PAD
+	HighOctetPreset                      rune = HOP
+	BreakPermittedHere                   rune = BPH
+	NoBreakHere                          rune = NBH
+	Index                                rune = IND
+	NextLine                             rune = NEL
+	StartOfSelectedArea                  rune = SSA
+	EndOfSelectedArea                    rune = ESA
+	CharacterTabulationSet               rune = HTS ; HorizontalTabulationSet               rune = HTS
+	CharacterTabulationWithJustification rune = HTJ ; HorizontalTabulationWithJustification rune = HTJ
+	LineTabulationSet                    rune = VTS ; VerticalTabulationSet                 rune = VTS
+	PartialLineDown                      rune = PLD ; PartialLineForward                    rune = PLD
+	PartialLineBackward                  rune = PLU ; PartialLineUp                         rune = PLU
+	ReverseIndex                         rune = RI  ; ReverseLineFeed                       rune = RI
+	SingleShiftTwo                       rune = SS2
+	SingleShiftThree                     rune = SS3
+	DeviceControlString                  rune = DCS
+	PrivateUseOne                        rune = PU1 ; PrivateUse1                           rune = PU1
+	PrivateUseTwo                        rune = PU2 ; PrivateUse2                           rune = PU2
+	SetTransmitState                     rune = STS
+	CancelCharacter                      rune = CCH
+	MessageWaiting                       rune = MW
+	StartOfGuardedArea                   rune = SPA ; StartOfProtectedArea                  rune = SPA
+	EndOfGuardedArea                     rune = EPA ; EndOfProtectedArea                    rune = EPA
+	StartOfString                        rune = SOS
+	SingleGraphicCharacterIntroducer     rune = SGC
+	SingleCharacterIntroducer            rune = SCI
+	ControlSequenceIntroducer            rune = CSI
+	StringTerminator                     rune = ST
+	OperatingSystemCommand               rune = OSC
+	PrivacyMessage                       rune = PM
+	ApplicationProgramCommand            rune = APC
 )
 
 const (
-	TransmitOn  = XON
-	TransmitOff = XOFF
+	TransmitOn  rune = XON
+	TransmitOff rune = XOFF
 )
